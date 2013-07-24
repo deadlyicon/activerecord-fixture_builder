44629aac09c13db112a3f030adad9db57d347041
Jared Grippe <jared@deadlyicon.com>
2013-07-23 18:00:46 -0700

    init

---
+++ lib/activerecord/fixture_builder.rb
@@ -0,0 +1,7 @@
+require "activerecord/fixture_builder/version"
+
+module Activerecord
+  module FixtureBuilder
+    # Your code goes here...
+  end
+end
---
+++ lib/activerecord/fixture_builder/version.rb
@@ -0,0 +1,5 @@
+module Activerecord
+  module FixtureBuilder
+    VERSION = "0.0.1"
+  end
+end
